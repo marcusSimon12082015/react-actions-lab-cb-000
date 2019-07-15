b04e589ae7286358358b74dbb406ad6205f9ef2b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-15 14:56:08 +0000

    Automatically backed up by Learn

--- components/Spreadsheet.js
+++ components/Spreadsheet.js
@@ -8,6 +8,13 @@ export default class Spreadsheet extends React.Component {
   constructor (props) {
     super(props);
     this.state = {};
+    this.handleAddColumn = actions.addColumn.bind(this);
+    this.handleAddRow = actions.addRow.bind(this);
+    this.handleRemoveColumn = actions.removeColumn.bind(this);
+    this.handleRemoveRow = actions.removeRow.bind(this);
+    this.handleChange = actions.changeCell.bind(this);
+    this.handleFocus = actions.focusCell.bind(this);
+    this.handleBlur = actions.blurCell.bind(this);
   }
 
   render () {
