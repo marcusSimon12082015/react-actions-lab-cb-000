d7e100899bf232b10d2b2c49b51c7720204aefb2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-15 15:31:12 +0000

    Automatically backed up by Learn

--- components/Table.js
+++ components/Table.js
@@ -5,7 +5,7 @@ import Row from './Row';
 
 export default class Table extends React.Component {
   render () {
-    this.props.table.map((row,i) => )
+    //this.props.table.map((row,i) => )
     return(<table className='table' onBlur={this.props.onBlur} onFocus={this.props.onFocus}><tbody></tbody></table>);
   }
 }
