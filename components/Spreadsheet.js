a0a1e0402062bcba8b2dfa309a58496a4ad65e0a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-15 15:20:13 +0000

    Automatically backed up by Learn

--- components/Spreadsheet.js
+++ components/Spreadsheet.js
@@ -32,7 +32,7 @@ export default class Spreadsheet extends React.Component {
           <button>Remove Row</button>
         </div>
 
-        <Table onBlur={this.handleBlur()}/>
+        <Table onBlur={this.handleBlur}/>
 
         <div className='spreadsheet__focused'>
         </div>
